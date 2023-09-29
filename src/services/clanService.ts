b4f3502ce64d436600e77c3192ef4c56d750e397
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-09-29 18:41:25 +0530

    Configure run scripts

--- src/services/clanService.ts
+++ src/services/clanService.ts
@@ -1,5 +1,5 @@
-export const filterInactivePlayers = async(teamId: string) => {
+export const filterInactivePlayers = async(clanId: string) => {
     const token = process.env.TOKEN;
 
-    return `Team ID: ${teamId}, Token: ${token}`;
+    return `Clan ID: ${clanId}`;
 }

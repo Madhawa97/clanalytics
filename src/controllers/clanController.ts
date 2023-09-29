d0fb09b24ed453dfb47dbcb697f194ebfb4c7f79
Madhawa Monarawila <madawammonarawila@gmail.com>
2023-09-29 20:02:54 +0530

    Add error handling for controller

--- src/controllers/clanController.ts
+++ src/controllers/clanController.ts
@@ -3,10 +3,15 @@ import { filterInactivePlayers } from '../services/clanService';
 
 export const getInactivePlayersController = async (req: Request, res: Response) => {
     const clanId = req.query.clanId as string;
-    if (clanId) {
-        const inactivePlayers = await filterInactivePlayers(clanId);
-        res.status(200).send(inactivePlayers);
-    } else {
-        res.status(404).send({ message: 'ClanId not found' });
+    try {
+        if (clanId) {
+            const inactivePlayers = await filterInactivePlayers(clanId);
+            res.status(200).send(inactivePlayers);
+        } else {
+            res.status(404).send({ message: 'ClanId not found' });
+        }
+    } catch (error) {
+        console.error('Error in getInactivePlayersController:', error);
+        res.status(500).send({ message: 'Internal Server Error' });
     }
 }
